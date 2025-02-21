241b0f1f3ef7875f5d1126d30834b229ffb231ab
Khushee2023 <154337721+Khushee2023@users.noreply.github.com>
2025-02-21 15:42:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -24,5 +24,4 @@ def onboarding():
     return render_template('onboarding.html')
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 10000))  
-    app.run(host="0.0.0.0", port=port, threaded=True)  
+    app.run(debug=True)
